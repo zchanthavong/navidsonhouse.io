491807d587c31bae688d182d0a4e319ac51c7d01
zchanthavong <zchanthavong@amarillocollege.com>
2026-04-06 16:48:50 -0500

    Add files via upload

--- scripts/estimating_cost.js
+++ scripts/estimating_cost.js
@@ -1,17 +1,24 @@
 const COST_PER_NIGHT = 60.00;
 const OFFICE_COST = 15.00;
 
+window.addEventListener("load", setupForm);
+
+function setupForm() {
+    document.getElementById("myEnd").value = 0;
+    document.getElementById("myTip").value = 0;
+    document.getElementById("officeAccess").value = false;
+    calcTotal();
+    document.getElementById("myEnd").onchange = calcTotal;
+    document.getElementById("myTip").onchange = calcTotal;
+    document.getElementById("officeAccess").onchange = calcTotal;
+}
+
 function formatCurrency(value) {
     return "$" + value.toFixed(2);
 }
 
-document.getElementById("myEnd").onchange("input", calcTotal);
-document.getElementById("myTip").onchange("input", calcTotal);
-document.getElementById("officeAccess").onchange("change", calcTotal);
-
 function calcTotal() {
     let total = 0;
-
     let days = parseFloat(document.getElementById("myEnd").value) || 0;
     let tip = parseFloat(document.getElementById("myTip").value) || 0;
     let boughtOffice = document.getElementById("officeAccess").checked;
@@ -21,6 +28,5 @@ function calcTotal() {
     total += dayCost;
     total += tip;
     total += boughtOffice ? OFFICE_COST : 0;
-
     document.getElementById("totalCost").innerHTML = formatCurrency(total);
 }
